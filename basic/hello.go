c5bd2cc5ec9a64c3868810555ef2ee5f6feb7d47
Mattias <walde87@msn.com>
2025-07-17 16:29:12 +0200

    First test and program pre-refactor

--- basic/hello.go
+++ basic/hello.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+func Hello(name string) string {
+	return "Hello, " + name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
+package main
+
+import (
+	"fmt"
+)
+
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
