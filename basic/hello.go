138b9086f54a21429d6d690ff6ac8f5e89c6dc2b
Mattias <walde87@msn.com>
2025-07-17 16:29:12 +0200

    First test and program pre-refactor

--- basic/hello.go
+++ basic/hello.go
@@ -4,13 +4,8 @@ import (
 	"fmt"
 )
 
-const englishHelloPrefix = "Hello, "
-
 func Hello(name string) string {
-	if name == "" {
-		name = "World"
-	}
-	return englishHelloPrefix + name
+	return "Hello, " + name
 }
 
 func main() {
