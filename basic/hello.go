b4122bd3ea5fa53d4d4d9859fab59185876a58a2
Mattias <walde87@msn.com>
2025-07-18 07:44:56 +0200

    Merge done

--- basic/hello.go
+++ basic/hello.go
@@ -16,18 +16,3 @@ func Hello(name string) string {
 func main() {
 	fmt.Println(Hello("world"))
 }
-package main
-
-import (
-	"fmt"
-)
-
-const englishHelloPrefix = "Hello, "
-
-func Hello(name string) string {
-	return englishHelloPrefix + name
-}
-
-func main() {
-	fmt.Println(Hello("world"))
-}
