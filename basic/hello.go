8ee6be697a18f639075d871e591bc820a6f4c867
Mattias <walde87@msn.com>
2025-07-18 08:20:22 +0200

    Added switch to replace many if-statements

--- basic/hello.go
+++ basic/hello.go
@@ -4,15 +4,33 @@ import (
 	"fmt"
 )
 
+const french = "French"
+const spanish = "Spanish"
+const swedish = "Swedish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const swedishHelloPrefix = "Hej, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case swedish:
+		prefix = swedishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world, ", ""))
 }
