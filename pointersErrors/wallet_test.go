d57f526ea6f984dd8083ecbf215f2e5b3fb16250
Mattias <walde87@msn.com>
2025-07-23 14:50:47 +0200

    Testing Deposit, Balance and Stringer funcs

---
+++ pointersErrors/wallet_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(Bitcoin(10))
+	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+	want := Bitcoin(20)
+
+	if got != want {
+		t.Errorf("got %d want %s", got, want)
+	}
+}
