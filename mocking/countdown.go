860b7429bd5b2f8c6621d1db98e5520e196ab527
Mattias <walde87@msn.com>
2025-07-29 12:15:58 +0200

    Mocking practice and refactoration

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
@@ -10,14 +11,38 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
-	for i := countdownStart; i > 0; i-- {
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := range countDownFrom(countdownStart) {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
