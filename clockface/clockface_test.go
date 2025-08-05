18e868693b46ff11dce80236a194e4006f849eb4
Mattias <walde87@msn.com>
2025-08-05 10:29:58 +0200

    folder namechange to not call math-package

---
+++ clockface/clockface_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/MaWalde/goWithTests.git/clockface"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
