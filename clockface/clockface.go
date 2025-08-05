45588f65e42f18835e4004b69e63ccd058575b13
Mattias <walde87@msn.com>
2025-08-05 11:53:24 +0200

    tests and functions for calculations

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,12 +1,10 @@
-package main
+package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
-type Point struct {
-	X float64
-	Y float64
-}
-
-func SecondHand(t time.Time) Point {
-	return Point{}
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MaWalde/goWithTests.git
 
 go 1.24.5
+
+require github.com/MaWalde/goWithTests v0.0.0-20250805083151-ed5d93494a6a
